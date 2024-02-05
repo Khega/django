05403e1e23944ff5e235c4be6a2dfa40a4730f69
Nikita <khegay.nik@gmail.com>
2024-02-05 17:25:14 +0600

    dd existing project files to it

--- product/views.py
+++ product/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, Photo
+from .forms import PhotoForm
 
-# Create your views here.
+def add_photo_to_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.product = product
+            photo.save()
+            return redirect('product_detail', product_id=product.id)
+    else:
+        form = PhotoForm()
+
+    return render(request, 'your_template/add_photo_to_product.html', {'form': form, 'product': product})
+
+def delete_photo(request, product_id, photo_id):
+    photo = get_object_or_404(Photo, pk=photo_id)
+    product_id = photo.product.id
+    photo.delete()
+    return redirect('product_detail', product_id=product_id)
--- src/settings.py
+++ src/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangodb',
-        'USER': 'root',
-        'PASSWORD': '111',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
